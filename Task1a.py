0449518159a811d8b60452cfbb003c9dfc403253
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-22 14:45:22 -0400

    Import modules and set input and output feature class filenames and paths

--- Task1a.py
+++ Task1a.py
@@ -7,3 +7,16 @@
 ## Created: Fall 2020
 ## Author: alison.stouffer@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
+
+#%% Import Modules and Set Variables
+
+#Import modules:
+import sys, os, arcpy
+
+#Set input feature class (FC) to streams shapefile in Data folder:
+inputFC = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data\\Streams.shp"
+
+#Set output feature class (FC) name to StrmBuff1km and store in Data folder:
+outputFC = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Scratch\\StrmBuff1km.shp"
+
+
