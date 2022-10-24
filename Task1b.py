c42aee9bdff5c73c7a143e36603493b0785db863
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-24 12:02:40 -0400

    Create Task1b.py

---
+++ Task1b.py
@@ -0,0 +1,32 @@
+##---------------------------------------------------------------------
+## Interactive Python Script for Buffering
+##
+## Description: Create 1,000 meter buffer from streams.
+##
+##
+## Created: Fall 2020
+## Author: alison.stouffer@duke.edu (for ENV859)
+##---------------------------------------------------------------------
+
+#%% Import Modules and Set Variables
+
+#Import modules:
+import sys, os, arcpy
+
+#Set input feature class (FC) to streams shapefile in Data folder:
+inputFC = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data\\Streams.shp"
+
+#Set output feature class (FC) name to StrmBuff1km and store in Data folder:
+outputFC = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Scratch\\StrmBuff1km.shp"
+
+#%% Create Buffer
+
+#Set buffer distance:
+streamBuffer = "1000 meters"
+
+#Create feature class with buffered streams:
+arcpy.Buffer_analysis(inputFC,outputFC,streamBuffer,'','','ALL')
+
+#%% Display Messages
+
+print(arcpy.GetMessages())
