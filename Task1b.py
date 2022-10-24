6d422eec601b1bd33aea88a66e66a58ea4e7ccb5
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-24 12:32:45 -0400

    Alter Task1a code for Task1b

--- Task1b.py
+++ Task1b.py
@@ -8,18 +8,25 @@
 ## Author: alison.stouffer@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
 
-#%% Import Modules and Set Variables
+#%% Import Modules and Set Environment
 
 #Import modules:
-import sys, os, arcpy
+import arcpy
+
+#Set workspace:
+arcpy.env.workspace = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data"
+
+#Allow output to be overwritten:
+arcpy.env.overwriteOutput = True
+
+#%%Set Input and Output Feature Classes
 
 #Set input feature class (FC) to streams shapefile in Data folder:
-inputFC = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data\\Streams.shp"
-
+inputFC = "streams.shp"
 #Set output feature class (FC) name to StrmBuff1km and store in Data folder:
 outputFC = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Scratch\\StrmBuff1km.shp"
 
-#%% Create Buffer
+#%%Create Buffer
 
 #Set buffer distance:
 streamBuffer = "1000 meters"
@@ -27,6 +34,6 @@ streamBuffer = "1000 meters"
 #Create feature class with buffered streams:
 arcpy.Buffer_analysis(inputFC,outputFC,streamBuffer,'','','ALL')
 
-#%% Display Messages
+#%%Display Messages
 
 print(arcpy.GetMessages())
