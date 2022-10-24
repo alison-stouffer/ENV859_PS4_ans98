182d6e8fd8cd6e0d112230fcb200bd3d8525382c
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-24 12:06:23 -0400

    Clean up Task1a code

--- Task1a.py
+++ Task1a.py
@@ -11,11 +11,10 @@
 #%% Import Modules and Set Variables
 
 #Import modules:
-import sys, os, arcpy
+import arcpy
 
 #Set input feature class (FC) to streams shapefile in Data folder:
 inputFC = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data\\Streams.shp"
-
 #Set output feature class (FC) name to StrmBuff1km and store in Data folder:
 outputFC = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Scratch\\StrmBuff1km.shp"
 
