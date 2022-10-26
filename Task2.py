9a337a84e25de50cc08fdde77b94b57d365f0e23
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-26 12:18:31 -0400

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,9 @@
+##---------------------------------------------------------------------
+## Selecting Road Types
+##
+## Description: Iterate through and select road features with desired code type.
+##
+##
+## Created: Fall 2020
+## Author: alison.stouffer@duke.edu (for ENV859)
+##---------------------------------------------------------------------
