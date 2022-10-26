6493c623a47b5ad8c4dceb880e747fff7a6a2d1c
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-26 12:26:12 -0400

    Set up working environment and define input feature class

--- Task2.py
+++ Task2.py
@@ -7,3 +7,21 @@
 ## Created: Fall 2020
 ## Author: alison.stouffer@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
+
+#%% Import Modules and Set Environment
+
+#Import modules:
+import arcpy
+
+#Set workspace:
+arcpy.env.workspace = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data"
+
+#Allow output to be overwritten:
+arcpy.env.overwriteOutput = True
+
+#%% Set Input Feature Classes
+
+#Set input feature class (FC) to roads shapefile in Data folder:
+inputFC = "Roads.shp"
+
+#%%
