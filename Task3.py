ac97f81c5b44fceed6f56e7f3f15053ad0506d3e
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-29 12:20:35 -0400

    Create Task3.py

---
+++ Task3.py
@@ -0,0 +1,9 @@
+##---------------------------------------------------------------------
+## Benthic Monitoring Sites in the Triangle
+##
+## Description: Split a point feature class into new, separte feature classes.
+##
+##
+## Created: Fall 2020
+## Author: alison.stouffer@duke.edu (for ENV859)
+##---------------------------------------------------------------------
