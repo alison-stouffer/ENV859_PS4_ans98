ecc82930c4cc7219e85ead927b73777e1598061d
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-29 12:33:05 -0400

    Set up working environment

--- Task3.py
+++ Task3.py
@@ -7,3 +7,26 @@
 ## Created: Fall 2020
 ## Author: alison.stouffer@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
+
+#%% Import Modules, Set Environment, and Check Product
+
+#Import modules:
+import sys, arcpy
+
+#Set workspace:
+arcpy.env.workspace = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data"
+
+#Check for advanced license:
+if arcpy.CheckProduct("ArcInfo") == "Available":
+    msg = 'ArcGIS for Desktop Advanced license is available'
+    print(msg)
+    sys.exit(msg)
+else:
+    msg = 'ArcGIS for Desktop Advanced license not available'
+    print(msg)
+    sys.exit(msg)
+    
+#Allow output to be overwritten:
+arcpy.env.overwriteOutput = True
+
+#%%
