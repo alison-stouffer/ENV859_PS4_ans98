72fa83d744d8061dbfca859aa224094a1230ae39
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-11-01 17:54:32 -0400

    Write Task4 code

--- Task4.py
+++ Task4.py
@@ -18,3 +18,33 @@ arcpy.env.workspace = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data"
 
 #Allow output to be overwritten:
 arcpy.env.overwriteOutput = True
+
+#%% User-Specified Dataset & Describe Object
+
+#Create parameter for user-specified dataset:
+dataset = sys.argv[1]
+#Create describe object from dataset:
+describe = arcpy.da.Describe(dataset)
+
+#%% ArcGIS Messages
+
+#Print dataset path:
+print(describe["catalogPath"])
+
+#Print extent of dataset:
+extent = describe["extent"]
+print("XMin: " + str(round(extent.XMin, 1)))
+print("YMin: " + str(round(extent.YMin, 1)))
+print("XMax: " + str(round(extent.XMax, 1)))
+print("YMax: " + str(round(extent.YMax, 1)))
+
+#Check data type:
+datasetType = describe["dataType"]
+#Print properties of feature class:
+if datasetType == "ShapeFile":
+    arcpy.AddWarning("Shapetype: " + describe["shapeType"])
+#Print properties of raster:
+else:
+    arcpy.AddWarning("Format: " + describe["format"])
+    arcpy.AddWarning("# of rows: " + str(describe["height"]))
+    arcpy.AddWarning("# of columns: " + str(describe["width"]))
