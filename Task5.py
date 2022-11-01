9f743af79ca86302e4ecab7a689d88bb4e71c46f
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-11-01 19:48:09 -0400

    Import modules and set workspace for Task5

--- Task5.py
+++ Task5.py
@@ -7,3 +7,15 @@
 ## Created: Fall 2020
 ## Author: alison.stouffer@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
+
+#%% Import Modules, Set Environment, and Check Product
+
+#Import modules:
+import sys, arcpy
+
+#Set workspace:
+arcpy.env.workspace = "V:\\_ProblemSets\\ENV859_PS4_ans98\\Data"
+
+#Allow output to be overwritten:
+arcpy.env.overwriteOutput = True
+
